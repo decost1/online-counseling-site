cc0b539c546fbb0fcf9a4481e9d2d869844b278d
Nagisa Komori <nagisa@decost.net>
2025-12-19 08:36:08 +0900

    Fix: include sanity client for production

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,8 +1,9 @@
-import { createClient } from '@sanity/client';
+// src/sanityClient.js
+import { createClient } from "@sanity/client";
 
 export const client = createClient({
   projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: import.meta.env.VITE_SANITY_DATASET,
-  apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
+  apiVersion: import.meta.env.VITE_SANITY_API_VERSION || "2023-10-01",
   useCdn: true,
 });
