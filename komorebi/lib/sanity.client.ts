aad439b988353ef3923e023f98de1584e5a56e57
Nagisa Komori <nagisa@decost.net>
2025-12-19 08:00:49 +0900

    Fix sanity env config

--- komorebi/lib/sanity.client.ts
+++ komorebi/lib/sanity.client.ts
@@ -1,9 +1,8 @@
-// lib/sanity.client.ts
-import {createClient} from '@sanity/client'
+import { createClient } from '@sanity/client'
 
 export const sanityClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!, // ibiu2qpi
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,     // production
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-01',
-  useCdn: true, // 公開データはCDN OK
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
+  apiVersion: import.meta.env.VITE_SANITY_API_VERSION || '2025-01-01',
+  useCdn: true,
 })
