e897f38d709fa17187d6c096180ba6907f9a83d7
Nagisa Komori <nagisa@decost.net>
2025-12-19 23:26:25 +0900

    Fix sanity client for Vite prod

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -4,6 +4,6 @@ import { createClient } from "@sanity/client";
 export const client = createClient({
   projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: import.meta.env.VITE_SANITY_DATASET,
-  apiVersion: import.meta.env.VITE_SANITY_API_VERSION || "2023-10-01",
+  apiVersion: import.meta.env.VITE_SANITY_API_VERSION || "2025-01-01",
   useCdn: true,
 });
